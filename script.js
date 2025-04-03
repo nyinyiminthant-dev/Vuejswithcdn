da69ac01a3c7d1d880a77c485b214a6849b2c91b
nyinyiminthant-dev <nnyi37389@gmail.com>
2025-04-03 20:22:58 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+    data: {
+        available: false,
+     nearby: false,
+    },
+    methods: {
+       
+    },
+    computed: {
+      compClasses: function() {
+        return {
+          'available': this.available,
+          'nearby': this.nearby
+        }
+      }
+    },
+        
+
+})
